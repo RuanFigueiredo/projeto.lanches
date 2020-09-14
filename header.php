28e3849d8374df258128f9326f26f1899bd0aac2
RuanFigueiredo <45396999+RuanFigueiredo@users.noreply.github.com>
2020-09-14 08:46:31 -0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<title>lanchonete Figueiredo's</title>
+<meta charset = UTF-8>
+
+
+<link rel = "stylesheet" href = "bootstrap.min.css">
+<link rel = "stylesheet" href = "//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+<link rel = "stylesheet" href = "style.css">
+<link rel = "stylesheet" href = "bootstrapboot.css">
+<link rel = "stylesheet" href = "bootstrap-grid.min.css">
+
+
+
+<body>
+    <header>
+    <div class= "container" >
+        <a href="?pagina=home"><img src ="" title = "Logo" alt="Logo"></a>
+        <div id="menu"></div>
+            <a href = "?pagina=bauru">Baurú </a>
+            <a href = "?pagina=cachorroquente">Cachorro quente</a>
+            <a href = "?pagina=batatafrita">Batata Frita </a>
+            <a href = "?pagina=pastel">Pastel </a>
+        </div>  
+    </div>      
+    </header>
+
+
+<div id= "conteudo" class = "container" >
+
+
+
+</head>
+</html>
+
