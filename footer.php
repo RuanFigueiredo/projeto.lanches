a2ca0c4007a02960e128740a1aff4765cdcda50a
RuanFigueiredo <45396999+RuanFigueiredo@users.noreply.github.com>
2020-09-14 08:45:20 -0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,22 @@
+</div>
+
+<footer>
+    <div class= "container">
+    ©2020 Lanchonete figueiredo's -  Todos os direitos reservados.
+</div>
+
+
+<script src="js/jquery.js"></script>
+<script src="//cdn.databases.net/1.10.19/js/jquery.databases.min.js"></script>
+
+<script>
+
+
+    $(document). ready (function)() {
+        $('#cursos').DataTable();
+        $('#alunos').DataTable();
+        $('#matriculas').DataTable();
+    } ) ;
+</footer>
+</body>
+</html>
